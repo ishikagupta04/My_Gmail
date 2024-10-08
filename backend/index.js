c948e8745074f78c463ca421eab9174bd0a5e66c
Ishika gupta <ishikag889@gmail.com>
2024-10-08 12:25:18 +0530

    add new

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'https://mernstack-gmailclone-main.onrender.com',
+    origin:'http://localhost:5173',
     credentials:true
 }
 app.use(cors(corsOptions));

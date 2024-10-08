c85478a7a31ea92537be6d48ee073b8dd5b3ac4b
Ishika gupta <ishikag889@gmail.com>
2024-10-08 11:14:43 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://earnest-elf-aaaddc.netlify.app',
     credentials:true
 }
 app.use(cors(corsOptions));
